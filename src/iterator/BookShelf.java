a60132838b9f05c81ed9d4e2368a1611a6b76964
kkoon9 <rndrnjs2003@gmail.com>
2022-03-08 22:58:27 +0900

    fix: Iterator 패턴에서 ArrayList로 변경
    Signed-off-by: kkoon9 <rndrnjs2003@gmail.com>

--- src/iterator/BookShelf.java
+++ src/iterator/BookShelf.java
@@ -1,24 +1,25 @@
 package iterator;
 
-public class BookShelf implements Aggregate {
-    private Book[] books;
-    private int last = 0;
+import java.util.ArrayList;
+import java.util.List;
 
-    public BookShelf(int maxsize) {
-        this.books = new Book[maxsize];
+public class BookShelf implements Aggregate {
+    private List<Book> books;
+
+    public BookShelf() {
+        this.books = new ArrayList<>();
     }
 
     public Book getBookAt(int index) {
-        return books[index];
+        return books.get(index);
     }
 
     public void appendBook(Book book) {
-        this.books[last] = book;
-        last++;
+        books.add(book);
     }
 
     public int getLength() {
-        return last;
+        return books.size();
     }
 
     @Override
--- src/iterator/Main.java
+++ src/iterator/Main.java
@@ -2,11 +2,12 @@ package iterator;
 
 public class Main {
     public static void main(String[] args) {
-        BookShelf bookShelf = new BookShelf(4);
+        BookShelf bookShelf = new BookShelf();
         bookShelf.appendBook(new Book("Spring of Toby"));
         bookShelf.appendBook(new Book("Effective Java"));
         bookShelf.appendBook(new Book("Unit Test"));
         bookShelf.appendBook(new Book("Kotlin IN ACTION"));
+        bookShelf.appendBook(new Book("Java IN ACTION"));
 
         Iterator it = bookShelf.iterator();
 
