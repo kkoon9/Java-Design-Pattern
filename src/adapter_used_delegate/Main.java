41f1350e0b779ffa193f3d9102f36e1f0a1fd0ea
kkoon9 <rndrnjs2003@gmail.com>
2022-03-09 21:09:12 +0900

    feat: 위임을 이용한 어댑터 패턴 에제 코드
    Signed-off-by: kkoon9 <rndrnjs2003@gmail.com>

---
+++ src/adapter_used_delegate/Main.java
@@ -0,0 +1,9 @@
+package adapter_used_delegate;
+
+public class Main {
+    public static void main(String[] args) {
+        Print p = new PrintBanner("Hello");
+        p.printWeak();
+        p.printStrong();
+    }
+}
