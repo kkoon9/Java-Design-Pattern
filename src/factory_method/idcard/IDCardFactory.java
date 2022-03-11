c389c8fed694c7a08aa394e0d2a432c59e252f31
kkoon9 <rndrnjs2003@gmail.com>
2022-03-11 20:53:03 +0900

    feat: 팩토리 메서드 패턴 예제 코드
    Signed-off-by: kkoon9 <rndrnjs2003@gmail.com>

---
+++ src/factory_method/idcard/IDCardFactory.java
@@ -0,0 +1,25 @@
+package factory_method.idcard;
+
+import factory_method.framework.Factory;
+import factory_method.framework.Product;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class IDCardFactory extends Factory {
+    private List<String> owners = new ArrayList<>();
+
+    @Override
+    protected Product createProduct(String owner) {
+        return new IDCard(owner);
+    }
+
+    @Override
+    protected void registerProduct(Product product) {
+        owners.add(((IDCard)product).getOwner());
+    }
+
+    public List<String> getOnwers() {
+        return owners;
+    }
+}
