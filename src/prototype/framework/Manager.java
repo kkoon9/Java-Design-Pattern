53bbea603f7c51087ba9591523f27f09bb9bb542
kkoon9 <rndrnjs2003@gmail.com>
2022-03-15 09:42:17 +0900

    feat: 프로토타입 패턴 예제 코드
    Signed-off-by: kkoon9 <rndrnjs2003@gmail.com>

---
+++ src/prototype/framework/Manager.java
@@ -0,0 +1,17 @@
+package prototype.framework;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Manager {
+    private HashMap showcase = new HashMap();
+
+    public void register(String name, Product proto) {
+        showcase.put(name, proto);
+    }
+
+    public Product create(String protoname) {
+        Product p = (Product) showcase.get(protoname);
+        return p.createClone();
+    }
+}
---
+++ src/prototype/framework/Product.java
@@ -0,0 +1,6 @@
+package prototype.framework;
+
+public interface Product extends Cloneable {
+    void use(String s);
+    Product createClone();
+}
