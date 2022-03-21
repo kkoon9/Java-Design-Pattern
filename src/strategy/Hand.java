c50cc98143731aaaf17c31251f7b1b3af96d2c47
kkoon9 <rndrnjs2003@gmail.com>
2022-03-21 22:27:18 +0900

    feat: 전략 패턴 예제 코드
    Signed-off-by: kkoon9 <rndrnjs2003@gmail.com>

---
+++ src/strategy/Hand.java
@@ -0,0 +1,44 @@
+package strategy;
+
+public class Hand {
+    public static final int HANDVALUE_GUU = 0; // 주먹
+    public static final int HANDVALUE_CHO = 1; // 가위
+    public static final int HANDVALUE_PAA = 2; // 보
+    public static final Hand[] hand = {
+            new Hand(HANDVALUE_GUU),
+            new Hand(HANDVALUE_CHO),
+            new Hand(HANDVALUE_PAA),
+    };
+
+    private static final String[] name = {
+            "주먹", "가위", "보"
+    };
+
+    private int handValue;
+    private Hand(int handValue) {
+        this.handValue = handValue;
+    }
+
+    public static Hand getHand(int handValue) {
+        return hand[handValue];
+    }
+
+    public boolean isStrongerThan(Hand h) { // this가 h를 이길 경우 true
+        return fight(h) == 1;
+    }
+
+    private int fight(Hand h) {
+        if(this == h) {
+            return 0;
+        } else if((this.handValue + 1) % 3 == h.handValue) {
+            return 1;
+        } else {
+            return -1;
+        }
+    }
+
+    public String toString() {
+        return name[handValue];
+    }
+
+}
