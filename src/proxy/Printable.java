bc8762abac0523fac4fcb966c5163cc219281d6d
kkoon9 <rndrnjs2003@gmail.com>
2022-04-18 19:37:41 +0900

    feat: 프록시 패턴 예제 코드
    Signed-off-by: kkoon9 <rndrnjs2003@gmail.com>

---
+++ src/proxy/Printable.java
@@ -0,0 +1,7 @@
+package proxy;
+
+public interface Printable {
+    void setPrinterName(String name);
+    String getPrinterName();
+    void print(String string);
+}
---
+++ src/proxy/Printer.java
@@ -0,0 +1,40 @@
+package proxy;
+
+public class Printer implements Printable {
+    private String name;
+
+    public Printer(String name) {
+        this.name = name;
+        heavyJob("Print의 인스턴스를 생성 중");
+    }
+
+    @Override
+    public void setPrinterName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String getPrinterName() {
+        return name;
+    }
+
+    @Override
+    public void print(String string) {
+        System.out.println("===" + name + "===");
+        System.out.println(string);
+    }
+
+    private void heavyJob(String msg) {
+        System.out.print(msg);
+        for (int i = 0; i < 5; i++) {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+            }
+            System.out.print(".");
+        }
+
+        System.out.println("완료.");
+    }
+
+}
