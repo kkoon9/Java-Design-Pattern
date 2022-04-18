3a3406d1442a750df9c6e722fb0bfb6a6a7d9122
kkoon9 <rndrnjs2003@gmail.com>
2022-04-18 21:29:36 +0900

    feat: 커맨드 패턴 예제 코드
    Signed-off-by: kkoon9 <rndrnjs2003@gmail.com>

---
+++ src/command/drawer/DrawCommand.java
@@ -0,0 +1,20 @@
+package command.drawer;
+
+import command.command.Command;
+
+import java.awt.*;
+
+public class DrawCommand implements Command {
+    protected Drawable drawable;
+    private Point position;
+
+    public DrawCommand(Drawable drawable, Point position) {
+        this.drawable = drawable;
+        this.position = position;
+    }
+
+    @Override
+    public void execute() {
+        drawable.draw(position.x, position.y);
+    }
+}
